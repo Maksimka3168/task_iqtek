1170c96722d81a15d36ef8c646701a4e99681e4c
Evtushevskiy Maksim <gfer-6@mail.ru>
2024-07-11 02:21:18 +0600

    Refactor IoC

--- src/app/utils/core/ioc.py
+++ src/app/utils/core/ioc.py
@@ -7,11 +7,8 @@ class IoC:
     def __init__(self):
         self._values = {}
 
-    def register(self, key: Type[T], value: Any):
-        self._values[key] = value
-
     def set(self, key: Type[T], value: Any):
-        self.register(key, value)
+        self._values[key] = value
 
     def get(self, key: Type[T]) -> T:
         if key not in self._values:
